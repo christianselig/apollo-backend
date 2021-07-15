2f34e20c5e6edc57437ad79d360a90bf148edecb
Andre Medeiros <me@andremedeiros.info>
2021-07-14 20:52:51 -0400

    more parsing

--- internal/reddit/types.go
+++ internal/reddit/types.go
@@ -70,3 +70,65 @@ func NewMeResponse(val *fastjson.Value) *MeResponse {
 
 	return mr
 }
+
+type Thing struct {
+	Kind        string  `json:"kind"`
+	ID          string  `json:"id"`
+	Type        string  `json:"type"`
+	Author      string  `json:"author"`
+	Subject     string  `json:"subject"`
+	Body        string  `json:"body"`
+	CreatedAt   float64 `json:"created_utc"`
+	Context     string  `json:"context"`
+	ParentID    string  `json:"parent_id"`
+	LinkTitle   string  `json:"link_title"`
+	Destination string  `json:"dest"`
+	Subreddit   string  `json:"subreddit"`
+}
+
+func NewThing(val *fastjson.Value) *Thing {
+	t := &Thing{}
+
+	t.Kind = string(val.GetStringBytes("kind"))
+
+	data := val.Get("data")
+
+	t.ID = string(data.GetStringBytes("id"))
+	t.Type = string(data.GetStringBytes("type"))
+	t.Author = string(data.GetStringBytes("author"))
+	t.Subject = string(data.GetStringBytes("subject"))
+	t.Body = string(data.GetStringBytes("body"))
+	t.CreatedAt = data.GetFloat64("created_utc")
+	t.Context = string(data.GetStringBytes("context"))
+	t.ParentID = string(data.GetStringBytes("parent_id"))
+	t.LinkTitle = string(data.GetStringBytes("link_title"))
+	t.Destination = string(data.GetStringBytes("dest"))
+	t.Subreddit = string(data.GetStringBytes("subreddit"))
+
+	return t
+}
+
+type ListingResponse struct {
+	Count    int
+	Children []*Thing
+	After    string
+	Before   string
+}
+
+func NewListingResponse(val *fastjson.Value) *ListingResponse {
+	lr := &ListingResponse{}
+
+	data := val.Get("data")
+	lr.After = string(data.GetStringBytes("after"))
+	lr.Before = string(data.GetStringBytes("before"))
+	lr.Count = data.GetInt("dist")
+	lr.Children = make([]*Thing, lr.Count)
+
+	children := data.GetArray("children")
+	for i := 0; i < lr.Count; i++ {
+		t := NewThing(children[i])
+		lr.Children[i] = t
+	}
+
+	return lr
+}
--- internal/reddit/types_test.go
+++ internal/reddit/types_test.go
@@ -22,6 +22,38 @@ func TestMeResponseParsing(t *testing.T) {
 	me := NewMeResponse(val)
 	assert.NotNil(t, me)
 
-	assert.Equal(t, "1ia22", me.ID)
-	assert.Equal(t, "changelog", me.Name)
+	assert.Equal(t, "xgeee", me.ID)
+	assert.Equal(t, "hugocat", me.Name)
+}
+
+func TestListingResponseParsing(t *testing.T) {
+	bb, err := ioutil.ReadFile("testdata/message_inbox.json")
+	assert.NoError(t, err)
+
+	val, err := parser.ParseBytes(bb)
+	assert.NoError(t, err)
+
+	l := NewListingResponse(val)
+	assert.NotNil(t, l)
+
+	assert.Equal(t, 25, l.Count)
+	assert.Equal(t, 25, len(l.Children))
+	assert.Equal(t, "t1_h470gjv", l.After)
+	assert.Equal(t, "", l.Before)
+
+	thing := l.Children[0]
+	assert.Equal(t, "t4", thing.Kind)
+	assert.Equal(t, "138z6ke", thing.ID)
+	assert.Equal(t, "unknown", thing.Type)
+	assert.Equal(t, "iamthatis", thing.Author)
+	assert.Equal(t, "how goes it", thing.Subject)
+	assert.Equal(t, "how are you today", thing.Body)
+	assert.Equal(t, 1626285395.0, thing.CreatedAt)
+	assert.Equal(t, "hugocat", thing.Destination)
+
+	thing = l.Children[6]
+	assert.Equal(t, "/r/calicosummer/comments/ngcapc/hello_i_am_a_cat/h4q5j98/?context=3", thing.Context)
+	assert.Equal(t, "t1_h46tec3", thing.ParentID)
+	assert.Equal(t, "hello i am a cat", thing.LinkTitle)
+	assert.Equal(t, "calicosummer", thing.Subreddit)
 }
