8e2448b95eb717b4c3e358e3599833ba0271bff1
Andre Medeiros <me@andremedeiros.info>
2021-07-27 15:28:10 -0400

    Tweak receipt check response

--- internal/api/receipt.go
+++ internal/api/receipt.go
@@ -6,7 +6,29 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
+const receiptResponse = `{
+    "products": [
+        {
+            "name": "ultra",
+            "status": "SANBOX",
+            "subscription_type": "SANDBOX"
+        },
+        {
+            "name": "pro",
+            "status": "SANDBOX"
+        },
+        {
+            "name": "community_icons",
+            "status": "SANDBOX"
+        },
+        {
+            "name": "spca",
+            "status": "SANDBOX"
+        }
+    ]
+}`
+
 func (a *api) checkReceiptHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	w.Write([]byte(`{"status_code": 11, "status_message": "Receipt is valid, lifetime subscription", "subscription_type": "lifetime"}`))
+	w.Write([]byte(receiptResponse))
 	w.WriteHeader(http.StatusOK)
 }
