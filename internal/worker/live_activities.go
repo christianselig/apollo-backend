b36a2cd32a1c585c66675fe8450c0e22cac614e3
Andre Medeiros <hello@andre.cool>
2022-10-26 21:08:22 -0400

    fix the notification metric for live activities

--- internal/worker/live_activities.go
+++ internal/worker/live_activities.go
@@ -314,7 +314,7 @@ func (lac *liveActivitiesConsumer) Consume(delivery rmq.Delivery) {
 
 		_ = lac.liveActivityRepo.Delete(ctx, at)
 	} else {
-		_ = lac.statsd.Incr("apns.live_activities.sent", []string{}, 1)
+		_ = lac.statsd.Incr("apns.notification.sent", []string{}, 1)
 		lac.logger.Info("sent notification",
 			zap.String("live_activity#apns_token", at),
 		)
